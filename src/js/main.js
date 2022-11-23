a7950431d13c50d0edb0839bdd31c5d9ebd31865
Maruf Mia <mmaruf1137@gmail.com>
2022-11-23 19:05:19 +0600

    added shop page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
+// theme
+const btnDark = document.querySelector('.btn-dark');
+const btnLight = document.querySelector('.btn-light');
+const html = document.querySelector('html');
+
+btnDark.addEventListener('click', () => {
+	html.classList.remove('light');
+	html.classList.add('dark');
+});
+
+btnLight.addEventListener('click', () => {
+	html.classList.remove('dark');
+	html.classList.add('light');
+});
+
+// shop pgae sidebar
+const closeCategoryBtn = document.querySelector('.btn-close-categories');
+const openCategoryBtn = document.querySelector('.btn-category');
+const shop = document.querySelector('.shop');
+
+if (shop) {
+	closeCategoryBtn.addEventListener('click', () => {
+		shop.classList.remove('show-category');
+	});
+	openCategoryBtn.addEventListener('click', () => {
+		shop.classList.add('show-category');
+	});
+}
+
+// swiper carousel
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	loop: true,
